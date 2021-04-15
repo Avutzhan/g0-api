23d4ca987226d791e71713d789a8c4776145b523
Dautov Avutzhan <sd3@vrcode.kz>
2021-04-15 15:13:39 +0600

    bookdata-api

---
+++ datastore/datastore.go
@@ -0,0 +1,13 @@
+package datastore
+
+import "api-test/loader"
+
+type BookStore interface {
+	Initialize()
+	SearchAuthor(author string, ratingOver, ratingBelow float64, limit, skip int) *[]*loader.BookData
+	SearchBook(bookName string, ratingOver, ratingBelow float64, limit, skip int) *[]*loader.BookData
+	SearchISBN(isbn string) *loader.BookData
+	CreateBook(book *loader.BookData) bool
+	DeleteBook(isbn string) bool
+	UpdateBook(isbn string, book *loader.BookData) bool
+}
